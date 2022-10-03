17c0fd2d9cf3da6af6e85315da9e654b65a5de5f
조 인영 iycho <iycho@spacewalk.tech>
2022-10-03 06:24:51 +0000

    make it traiend

--- core/utils.py
+++ core/utils.py
@@ -6,8 +6,9 @@ from addict import Dict
 
 def apply_trace_model_mode(mode=False):
     def _apply_trace_model_mode(m):
-        if hasattr(m, 'trace_model'):
+        if hasattr(m, "trace_model"):
             m.trace_model = mode
+
     return _apply_trace_model_mode
 
 
@@ -17,7 +18,7 @@ def tensor_to_img(t, normalize=True, range=(-1, 1), to_numpy=True, rgb2bgr=True)
         t.add_(-range[0]).div_(range[1] - range[0] + 1e-5)
     img = t.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0)
     if to_numpy:
-        img = img.to('cpu', torch.uint8).numpy()
+        img = img.to("cpu", torch.uint8).numpy()
     if rgb2bgr:
         img = img[:, :, ::-1]
     return img
@@ -38,7 +39,7 @@ def load_cfg(path):
 
 
 def save_cfg(path, cfg):
-    with open(path, 'w') as stream:
+    with open(path, "w") as stream:
         json.dump(cfg, stream, indent=4)
     return cfg
 
@@ -53,6 +54,7 @@ def select_weights(ckpt, prefix="student."):
 
 def load_weights(target, source_state):
     from collections import OrderedDict
+
     new_dict = OrderedDict()
     for k, v in target.state_dict().items():
         if k in source_state and v.size() == source_state[k].size():
--- train.py
+++ train.py
@@ -7,10 +7,10 @@ from core.utils import load_cfg, load_weights, select_weights
 from core.distiller import Distiller
 from core.model_zoo import model_zoo
 
+
 def build_logger(cfg):
-    return getattr(pl_loggers, cfg.type)(
-        **cfg.params
-    )
+    return getattr(pl_loggers, cfg.type)(**cfg.params)
+
 
 def main(args):
     cfg = load_cfg(args.cfg)
@@ -26,7 +26,7 @@ def main(args):
         verbose=True,
         monitor=cfg.trainer.monitor,
         mode=cfg.trainer.monitor_mode,
-        prefix=''
+        prefix="",
     )
     trainer = pl.Trainer(
         gpus=args.gpus,
@@ -35,7 +35,7 @@ def main(args):
         distributed_backend=args.distributed_backend,
         checkpoint_callback=checkpoint_callback,
         val_check_interval=args.val_check_interval,
-        logger=logger
+        logger=logger,
     )
     if args.export_model is None:
         trainer.fit(distiller)
@@ -51,15 +51,35 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # pipeline configure
     parser.add_argument("--gpus", type=int, default=0, help="number of available GPUs")
-    parser.add_argument('--distributed-backend', type=str, default="ddp", choices=('dp', 'ddp', 'ddp2'),
-                        help='supports three options dp, ddp, ddp2')
-    parser.add_argument("--checkpoint_dir", type=str, default=None, help="path to checkpoint_dir")
-    parser.add_argument("--val-check-interval", type=int, default=500, help="validation check interval")
-    parser.add_argument("--grad_batches", type=int, default=1, help="number of batches to accumulate")
+    parser.add_argument(
+        "--distributed-backend",
+        type=str,
+        default="ddp",
+        choices=("dp", "ddp", "ddp2"),
+        help="supports three options dp, ddp, ddp2",
+    )
+    parser.add_argument(
+        "--checkpoint_dir", type=str, default=None, help="path to checkpoint_dir"
+    )
+    parser.add_argument(
+        "--val-check-interval", type=int, default=500, help="validation check interval"
+    )
+    parser.add_argument(
+        "--grad_batches", type=int, default=1, help="number of batches to accumulate"
+    )
     parser.add_argument("--ckpt", type=str, default=None, help="path to checkpoint")
     parser.add_argument("--cfg", type=str, help="path to config file")
-    parser.add_argument("--export-model", type=str, default=None, help="export model for deploy. ['onnx', 'coreml']")
-    parser.add_argument("--export-dir", type=str, default="./", help="path to export model")
-    parser.add_argument("--export-w-plus", action='store_true', help="export with W+ space")
+    parser.add_argument(
+        "--export-model",
+        type=str,
+        default=None,
+        help="export model for deploy. ['onnx', 'coreml']",
+    )
+    parser.add_argument(
+        "--export-dir", type=str, default="./", help="path to export model"
+    )
+    parser.add_argument(
+        "--export-w-plus", action="store_true", help="export with W+ space"
+    )
     args = parser.parse_args()
     main(args)
